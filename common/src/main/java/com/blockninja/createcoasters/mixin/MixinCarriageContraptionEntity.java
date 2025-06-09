0f2ec8e0602c21c500f83bd3925888a193d0b381
blockninja124 <haganlgray@gmail.com>
2025-06-09 18:23:43 -0400

    bump version

--- common/src/main/java/com/blockninja/createcoasters/mixin/MixinCarriageContraptionEntity.java
+++ common/src/main/java/com/blockninja/createcoasters/mixin/MixinCarriageContraptionEntity.java
@@ -1,16 +1,10 @@
 package com.blockninja.createcoasters.mixin;
 
 import com.blockninja.createcoasters.ContraptionEntityExtraAccess;
-import com.simibubi.create.content.trains.entity.Carriage;
 import com.simibubi.create.content.trains.entity.CarriageContraptionEntity;
-import com.simibubi.create.content.trains.entity.CarriageSounds;
 import net.minecraft.world.item.DyeColor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.ArrayList;
 
