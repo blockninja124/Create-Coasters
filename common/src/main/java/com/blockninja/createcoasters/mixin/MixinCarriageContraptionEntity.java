b7ff12d46701896ae58763c8b5faba2dc2697f51
blockninja124 <haganlgray@gmail.com>
2025-06-09 19:33:50 -0400

    Remove extra prints

--- common/src/main/java/com/blockninja/createcoasters/mixin/MixinCarriageContraptionEntity.java
+++ common/src/main/java/com/blockninja/createcoasters/mixin/MixinCarriageContraptionEntity.java
@@ -33,8 +33,8 @@ public class MixinCarriageContraptionEntity implements ContraptionEntityExtraAcc
 
     @Override
     public void setDoSounds(boolean doSoundsNew) {
-        System.out.println("Set do sounds on:");
-        System.out.println(this);
+        //System.out.println("Set do sounds on:");
+        //System.out.println(this);
         doSounds = doSoundsNew;
     }
 }
