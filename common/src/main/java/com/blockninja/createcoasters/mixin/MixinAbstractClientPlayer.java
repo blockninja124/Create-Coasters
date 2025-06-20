d94c20f850ee9d13a0bd7d13d7fc537dd52be58d
blockninja124 <haganlgray@gmail.com>
2025-06-20 14:04:14 -0400

    Add camera zoom at high speed on trains

---
+++ common/src/main/java/com/blockninja/createcoasters/mixin/MixinAbstractClientPlayer.java
@@ -0,0 +1,52 @@
+package com.blockninja.createcoasters.mixin;
+
+import com.simibubi.create.content.contraptions.AbstractContraptionEntity;
+import com.simibubi.create.content.trains.entity.CarriageContraptionEntity;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.util.Mth;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractClientPlayer.class)
+public class MixinAbstractClientPlayer {
+    @Inject(method = "getFieldOfViewModifier", at = @At("RETURN"), cancellable = true)
+    private void getFOVMod(CallbackInfoReturnable<Float> cir) {
+        AbstractClientPlayer self = (AbstractClientPlayer) (Object) this;
+        float currentFOV = cir.getReturnValue();
+        if (self.getVehicle() instanceof CarriageContraptionEntity carriageContraptionEntity) {
+            //System.out.println(carriageContraptionEntity.getCarriage().train.speed);
+            /*AbstractContraptionEntity.ContraptionRotationState rotState = carriageContraptionEntity.getRotationState();
+
+            carriageContraptionEntity.getViewVector(1.0f);
+
+            Vec3 forward = new Vec3(carriageContraptionEntity.yaw, carriageContraptionEntity.pitch, 0);
+
+
+            float pitch = ((ICarriageBogeyAccessor) carriageContraptionEntity.getCarriage().leadingBogey()).getPitch().getValue(); // up/down
+            float yaw = ((ICarriageBogeyAccessor) carriageContraptionEntity.getCarriage().leadingBogey()).getYaw().getValue();   // left/right
+
+            float pitchRad = (float) Math.toRadians(pitch);
+            float yawRad = (float) Math.toRadians(yaw);
+
+            double x = -Math.sin(yawRad) * Math.cos(pitchRad);
+            double y = -Math.sin(pitchRad);
+            double z =  Math.cos(yawRad) * Math.cos(pitchRad);
+
+            Vec3 entityLook = new Vec3(x, y, z).normalize();
+
+            double angleDiff = self.getLookAngle().dot(entityLook); // angular distance*/
+
+            double dx = carriageContraptionEntity.getX() - carriageContraptionEntity.xOld;
+            double dy = carriageContraptionEntity.getY() - carriageContraptionEntity.yOld;
+            double dz = carriageContraptionEntity.getZ() - carriageContraptionEntity.zOld;
+
+            float budget_speed = (float) Math.sqrt(dx * dx + dy * dy + dz * dz); // per tick
+
+            cir.setReturnValue((float) (currentFOV * (1+(Math.abs(budget_speed/5)))));
+        }
+    }
+}
